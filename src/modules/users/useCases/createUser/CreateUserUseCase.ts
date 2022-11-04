ec474baa617e31e9046dd9838d168a05728fcda6
igortargino <igortargino01@gmail.com>
2022-11-04 20:32:21 -0300

    fix: tests

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const EmailIsAlreadyTaken = this.usersRepository.findByEmail(email);
+
+    if (EmailIsAlreadyTaken) {
+      throw new Error("E-mail is already taken");
+    }
+
+    const user = this.usersRepository.create({ email, name });
+
+    return user;
   }
 }
 
